4ea1bbf4a937bfa447da902a400b1d380ee3a447
Kunal Juvvala <juvvala123.456@gmail.com>
2025-12-03 16:56:20 -0500

    Kunal 12.3 Latest Commit

--- ui/view/LoginSecondView.java
+++ ui/view/LoginSecondView.java
@@ -5,20 +5,22 @@ import java.util.Scanner;
 import com.ourblogs.entity.Account;
 
 public class LoginSecondView {
-	public static void displayBlogMenu(String message) {
-		System.out.println("1 Create Blog");
-		System.out.println("2 View Blogs");
-		System.out.println("3 Add Readers");
-		System.out.println("4 Read Blogs");
-		System.out.println("5 Exit");
-	}
-	
-	public static int getUserChoice(Scanner scanner) {
-      try {
-        return Integer.parseInt(scanner.nextLine());
-      } catch (NumberFormatException e) {
-        return -1;
-      }
-	}
+  public static void displayBlogMenu(String message) {
+    System.out.println("1 Create Blog");
+    System.out.println("2 View Blogs");
+    System.out.println("3 Add Readers");
+    System.out.println("4 Read Blogs");
+    System.out.println("5 Edit Blogs");
+    System.out.println("6 Delete Blogs");
+    System.out.println("7 Exit");
+  }
+
+  public static int getUserChoice(Scanner scanner) {
+    try {
+      return Integer.parseInt(scanner.nextLine());
+    } catch (NumberFormatException e) {
+      return -1;
+    }
+  }
 }
 
